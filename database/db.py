f8f3e8d1b9e6baa238c4cfc4f28d730dc9dd5393
bohdan-orlyk <lollikwinner@gmail.com>
2024-04-23 14:05:23 +0200

    add config from .env

--- database/db.py
+++ database/db.py
@@ -1,12 +1,16 @@
 from sqlmodel import create_engine, SQLModel, Session
+from database.db_config import get_db_config
 
 from api.v1.library import models
 
 
-sqlite_file_name = "zhekas.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# sqlite_file_name = "zhekas.db"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, echo=True)  # TODO change later to False
+db_config = get_db_config()
+sqlite_url = f"{db_config.db_url_prefix}{db_config.db_name}"
+
+engine = create_engine(sqlite_url, echo=db_config.db_echo_mode)
 
 
 def create_tables():
--- database/db_config.py
+++ database/db_config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class DBSettings(BaseSettings):
     db_name: str
     db_url_prefix: str
+    db_echo_mode: bool = False
 
 
 def get_db_config() -> DBSettings:
